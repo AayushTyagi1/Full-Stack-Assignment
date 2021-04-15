c803927c2dde18487664eb5cc8724ac94fcc03ec
Aayush Tyagi <tyagiaayush5@gmail.com>
2021-04-15 22:39:05 +0530

    Assignment 5

--- Assignment-3/Q3.php
+++ Assignment-3/Q3.php
@@ -6,7 +6,7 @@ include('config.php');
 if(isset($_POST['submit'])){
     $username = $_POST['username'];
     $email = $_POST['email'];
-    $gender = $_POST['gender'];
+    $gender = $_POST['gender']
 	$city = $_POST['city'];
 	$sql =  "INSERT INTO `users` (`username`, `email`, `gender`, `city`) VALUES ('$username', '$email', '$gender', '$city')";
 			$a = mysqli_query($conn , $sql);
